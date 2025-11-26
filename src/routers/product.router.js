59cb8edca929b4e16e587beea9429746aa4a5467
ariekas <ariariekas@gmail.com>
2025-11-26 09:17:10 +0700

    fix: fix error cant generate image

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,14 +1,17 @@
 const express = require("express");
-const  productController = require("../controllers/product.controller");
+const productController = require("../controllers/product.controller");
+const upload = require("./../../lib/middelware/uploadImage");
 const router = express.Router();
-const multer = require("multer");
-const upload = multer({dest : "uploads/"});
 
 router.get("/", productController.listProducts);
 router.post("/", productController.CreateProduct);
 router.patch("/:id", productController.EditProduct);
 router.get("/:id", productController.detailProduct);
 router.delete("/:id", productController.deleteProduct);
-router.post("/upload/image/:id", upload.single("image"), productController.uploadImage);
+router.post(
+  "/upload/image/:id",
+  upload.single("image"),
+  productController.uploadImage
+);
 
 module.exports = router;
